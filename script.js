1b78fb6b21bd2460e5a66861f0552d33dcde0dd4
anaborgestri <aborges318@gmail.com>
2024-01-15 15:28:46 +0100

    Start project - created ht,l, css and js files

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function showAlert(event) {
-  if (event.target.value == "red") {
-    alert("❤️");
-  }
-  if (event.target.value == "yellow") {
-    alert("💛");
-  }
-  if (event.target.value == "blue") {
-    alert("💙");
+let parisTime = moment()
+  .tz("Europe/Paris")
+  .format("[It is] dddd, MMMM D, YYYY h:m A [in Europe/Paris]");
+
+let tokyoTime = moment()
+  .tz("Asia/Tokyo")
+  .format("[It is] dddd, MMMM D, YYYY h:m A [in Asia/Tokyo]");
+
+let sydneyTime = moment()
+  .tz("Australia/Sydney")
+  .format("[It is] dddd, MMMM D, YYYY h:m A [in Australia/Sydney]");
+
+function ShowTime(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "paris") {
+      alert(`${parisTime}`);
+    }
+    if (event.target.value === "tokyo") {
+      alert(`${tokyoTime}`);
+    }
+    if (event.target.value === "sydney") {
+      alert(`${sydneyTime}`);
+    }
   }
 }
 
-let colorsSelect = document.querySelector("#colors");
-colorsSelect.addEventListener("change", showAlert);
+let countrySelection = document.querySelector("#countries");
+countrySelection.addEventListener("change", ShowTime);
