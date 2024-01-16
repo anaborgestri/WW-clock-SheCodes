3750c3f7fd2c709b8baa3effc69f0e87a12ced3f
anaborgestri <aborges318@gmail.com>
2024-01-16 19:37:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function refreshTime() {
   let tokyoTime = document.querySelector("#onetime");
   tokyoTime.innerHTML = moment()
-    .tz("Asia / Tokyo")
+    .tz("Asia/Tokyo")
     .format("hh:mm:ss [<small>]A[</small>]");
   let tokyoDate = document.querySelector("#onedate");
-  tokyoDate.innerHTML = moment().tz("Asia / Tokyo").format("Do MMMM YYYY");
+  tokyoDate.innerHTML = moment().tz("Asia/Tokyo").format("Do MMMM YYYY");
 
   let saopauloTime = document.querySelector("#twotime");
   saopauloTime.innerHTML = moment()
@@ -17,10 +17,12 @@ function refreshTime() {
 
   let barcelonaTime = document.querySelector("#threetime");
   barcelonaTime.innerHTML = moment()
-    .tz("Europe/Madrid")
+    .tz("Europe / Madrid")
     .format("hh:mm:ss [<small>]A[</small>]");
   let barcelonaDate = document.querySelector("#threedate");
-  barcelonaDate.innerHTML = moment().tz("Europe/Madrid").format("Do MMMM YYYY");
+  barcelonaDate.innerHTML = moment()
+    .tz("Europe / Madrid")
+    .format("Do MMMM YYYY");
 }
 refreshTime();
 setInterval(refreshTime, 1000);
