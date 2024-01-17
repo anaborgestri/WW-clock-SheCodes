311c3f643609ce61d2fbffcee1fdad4837aad09b
anaborgestri <aborges318@gmail.com>
2024-01-17 11:15:50 +0100

    set up js select city

--- script.js
+++ script.js
@@ -17,12 +17,40 @@ function refreshTime() {
 
   let barcelonaTime = document.querySelector("#threetime");
   barcelonaTime.innerHTML = moment()
-    .tz("Europe / Madrid")
+    .tz("Europe/Madrid")
     .format("hh:mm:ss [<small>]A[</small>]");
   let barcelonaDate = document.querySelector("#threedate");
-  barcelonaDate.innerHTML = moment()
-    .tz("Europe / Madrid")
-    .format("D MMMM YYYY");
+  barcelonaDate.innerHTML = moment().tz("Europe/Madrid").format("D MMMM YYYY");
 }
 refreshTime();
 setInterval(refreshTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector(".cities");
+  citiesElement.innerHTML = cityTimeZone;
+  citiesElement.innerHTML = `<h2>${cityName}</h2>
+          <div class="city-one-details">
+            <div class="city-one-weather">
+              <div class="weather-icon">
+                ☀️
+                <span class="weather-temperature">9<strong>°C</strong> </span>
+              </div>
+            </div>
+            <div class="city-one-time">
+              <div class="time" id="onetime">${cityTime.format(
+                "hh:mm:ss [<small>]A[</small>]"
+              )}</div>
+              <div class="date" id="onedate">${cityTime.format(
+                "D MMMM YYYY"
+              )}</div>
+            </div>
+          </div>
+          <div class="weather-description">clear sunshine</div>
+        </div>`;
+}
+
+let selectCity = document.querySelector("#cities-select");
+selectCity.addEventListener("change", updateCity);
